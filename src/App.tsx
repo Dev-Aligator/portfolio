5fcf6d17b0c3645b193c6e0747d674342f8a670f
Dev-Aligator <devaligator1004@gmail.com>
2023-07-18 17:24:18 +0700

    Creating Footer and Adding animate.css, react-on-screen lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Banner from "./components/Banner";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
@@ -13,6 +14,7 @@ function App() {
       <Skills></Skills>
       <Projects></Projects>
       <Contact></Contact>
+      <Footer></Footer>
     </div>
   );
 }
