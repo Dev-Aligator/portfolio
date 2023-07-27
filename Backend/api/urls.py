6e8e4b2f8551bf8c491960dd6b57390b0f4ae30d
Dev-Aligator <devaligator1004@gmail.com>
2023-07-27 13:16:28 +0700

    Move the Get Banner Data url to /api/get/banner

--- Backend/api/urls.py
+++ Backend/api/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.getData),
-    path('add/', views.addItem),
+    path('get/banner/', views.getBannerData),
 ]
--- Backend/api/views.py
+++ Backend/api/views.py
@@ -4,16 +4,9 @@ from base.models import Banner, Role
 from .serializers import BannerSerializer
 
 @api_view(['GET'])
-def getData(request):
+def getBannerData(request):
     banner = Banner.objects.first()
     serializer = BannerSerializer(banner)
     return Response(serializer.data)
 
 
-@api_view(['POST'])
-def addItem(request):
-    serializer = BannerSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-
-    return Response(serializer.data)
