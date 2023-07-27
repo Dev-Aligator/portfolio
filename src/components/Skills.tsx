be6096337228e75470f76e858ee6169c22bae329
Dev-Aligator <devaligator1004@gmail.com>
2023-07-27 21:20:02 +0700

    Discard unused files

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,9 +1,6 @@
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import { Container, Col, Row } from "react-bootstrap";
-import meter1 from "../assets/img/meter1.svg";
-import meter2 from "../assets/img/meter2.svg";
-import meter3 from "../assets/img/meter3.svg";
 import colorSharp from "../assets/img/color-sharp.png";
 import SkillBox from "./SkillBox";
 import * as Models from "../DataInterfaces";
@@ -52,6 +49,7 @@ const Skills: React.FC<SkillProps> = ({ skillData }) => {
                     skillPercentage={skill.skillPercentage}
                   ></SkillBox>
                 ))}
+                <span></span>
               </Carousel>
             </div>
           </Col>
