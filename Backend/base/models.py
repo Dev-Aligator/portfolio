330c5090648712b0c22556a405fb7e511ae57f19
Dev-Aligator <devaligator1004@gmail.com>
2023-07-28 20:42:41 +0700

    Apply ImageField for imgUrl and adding __str__ function for some models

--- Backend/base/models.py
+++ Backend/base/models.py
@@ -12,14 +12,23 @@ class Role(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     banner = models.ForeignKey(Banner, on_delete=models.CASCADE, related_name="roles")
 
+    def __str__(self):
+        return self.roleName
+
 class Skill(models.Model):
     skillPercentage = models.PositiveSmallIntegerField(default=90)
     stopColor1 = models.CharField(max_length=20, null=True, blank=True)
     stopColor2 = models.CharField(max_length=20, null=True, blank=True)    
     skillName = models.CharField(max_length=100, unique=True)
 
+    def __str__(self):
+        return self.skillName
+
 class Project(models.Model):
-    title = models.CharField(max_length=200, unique=True);
-    description = models.CharField(max_length=200);
-    imgUrl = models.CharField(max_length=200, unique=True);
-    tabIndex = models.PositiveSmallIntegerField(default=1);
+    title = models.CharField(max_length=200, unique=True)
+    description = models.CharField(max_length=200)
+    imgUrl = models.ImageField(upload_to="images/")
+    tabIndex = models.PositiveSmallIntegerField(default=1)
+    
+    def __str__(self):
+        return self.title
